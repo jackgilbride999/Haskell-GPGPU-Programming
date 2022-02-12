0aad1f8675c8f48b6e82da0acecd860a98c4a927
Jack Joseph Gilbride <gilbridj@msc-linux-sls-043.scss.tcd.ie>
2022-02-12 13:51:41 +0000

    Transfer Config and World from accelerate-example. Error on Options type

---
+++ Config.hs
@@ -0,0 +1,43 @@
+-- Adapted from Mandelbrot accelerate example at https://github.com/AccelerateHS/accelerate-examples/blob/a973ee423b5eadda6ef2e2504d2383f625e49821/examples/mandelbrot/Config.hs
+
+{-# LANGUAGE TemplateHaskell #-}
+
+module Config where
+
+import Data.Label
+import System.Console.GetOpt
+
+data Config = Config
+    {
+            _configWidth    :: Int
+        ,   _configHeight   :: Int
+        ,   _configFilePath :: Maybe FilePath
+    }
+    deriving Show
+
+$(mkLabels [''Config])
+
+defaults :: Config
+defaults = Config
+    {   _configWidth  = 800
+    ,   _configHeight = 600
+    ,   _configFilePath = Nothing
+    }
+
+options :: [OptDescr (Config -> Config)]
+options = 
+    [ Option []   ["width"]   (ReqArg (set configWidth . read) "INT")     "visualisation width (800)"
+    , Option []   ["height"]  (ReqArg (set configHeight . read) "INT")    "visualisation height (600)"
+    , Option []   ["bmp"]     (ReqArg (set configFilePath . Just) "FILE") "save image to file"
+    ]
+
+header :: [String]
+header =
+    [ "Usage: accelerate-mandelbrot [OPTIONS]"]
+
+footer = 
+    [
+        "Runtime usage:"
+    ,   "   ESC         quit"
+    ,   "..."
+    ]
